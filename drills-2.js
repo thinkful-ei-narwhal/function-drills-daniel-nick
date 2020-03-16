4b4b1fbd0e397443ab2ecb0afc9c7fda03eb0a7d
nevelson <nevelson92@gmail.com>
2020-03-16 16:14:09 -0400

    For loop

--- drills-2.js
+++ drills-2.js
@@ -27,3 +27,32 @@ function beyond(num) {
 }
 
 beyond(0);
+
+//Cracking the code
+
+function decode(codedSentence) {
+  const wordArray = codedSentence.split(' ');
+  let decodedMessage = '';
+  for (let i = 0; i < wordArray.length; i++) {
+
+    if (wordArray[i][0] === 'a') {
+      decodedMessage += wordArray[i][1];
+    }
+    else if (wordArray[i][0] === 'b') {
+      decodedMessage += wordArray[i][2];
+    }
+    else if (wordArray[i][0] === 'c') {
+      decodedMessage += wordArray[i][3];
+    }
+    else if (wordArray[i][0] === 'd') {
+      decodedMessage += wordArray[i][4];
+    }
+    else {
+      decodedMessage += ' ';
+    }
+  }
+  return decodedMessage;
+}
+
+console.log(decode('craft block argon meter bells brown croon droop'));
+//For loop
