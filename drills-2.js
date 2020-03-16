f2c2c2e15ac0a0a9625873c68a83530420488890
DanielJSottile <danielsottile@gmail.com>
2020-03-16 15:59:38 -0400

    infinity

--- drills-2.js
+++ drills-2.js
@@ -1,8 +1,29 @@
 'use strict';
 
+// Jedi Name
+
 function jediName(firstName, lastName) {
   const jediFullName = lastName.slice(0, 3) + firstName.slice(0, 2);
   return jediFullName;
 }
 
 console.log(jediName('Bobin', 'jobe'));
+
+// To Infinity
+
+function beyond(num) {
+  if (num < 0) {
+    console.log('To negative infinity');
+  }
+  if (num > 0) {
+    console.log('To infinity');
+  }
+  if (num === Infinity || num === -Infinity) {
+    console.log('And beyond');
+  }
+  if (num === 0) {
+    console.log('Staying home');
+  }
+}
+
+beyond(0);
