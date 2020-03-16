05003f4fa8b2610db5d17be30fceaae65327644b
DanielJSottile <danielsottile@gmail.com>
2020-03-16 15:02:25 -0400

    3

--- drills-1.js
+++ drills-1.js
@@ -1,9 +1,9 @@
 'use strict';
 
-// 1
 
 function createGreeting(name, age) {
-  console.log(`Hi, my name is ${name} and I'm ${age}`);
+  return `Hi, my name is ${name} and I'm ${age}`;
 }
 
-createGreeting();
+const greeting1 = createGreeting();
+console.log(greeting1);
