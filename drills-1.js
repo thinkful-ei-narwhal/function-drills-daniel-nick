364d3221dff21c1b1d4baa87133c615cfa1a6891
nevelson <nevelson92@gmail.com>
2020-03-16 15:05:01 -0400

    4

--- drills-1.js
+++ drills-1.js
@@ -1,8 +1,8 @@
 'use strict';
 
-
 function createGreeting(name, age) {
-  return `Hi, my name is ${name} and I'm ${age}`;
+  const yearOfBirth = 2020 - age;
+  return `Hi, my name is ${name} and I'm ${age}. I was born in ${yearOfBirth}.`;
 }
 
 const greeting1 = createGreeting();
