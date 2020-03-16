8a350cc77d7323f3214fa0b687051097dd45d1bc
DanielJSottile <danielsottile@gmail.com>
2020-03-16 15:30:48 -0400

    final solution

--- drills-1.js
+++ drills-1.js
@@ -1,21 +1,30 @@
 'use strict';
 
 function getYearOfBirth(age) {
-  if (age < 0) {
-    throw new Error('Age can not be negative.');
-  }
   return 2020 - age;
 }
 
 function createGreeting(name, age) {
-  const yob = getYearOfBirth;
-  return `Hi, my name is ${name} and I'm ${age}. I was born in ${yob(age)}.`;
+  if (name ===  undefined || age === 'undefined') {
+    throw new Error('Arguments not valid.');
+  }
+
+  if (age < 0) {
+    throw new Error('Age can not be negative.');
+  }
+
+  if (typeof age !== 'number') {
+    throw new TypeError('Age must be a number');
+  }
+
+  const yob = getYearOfBirth(age);
+  return `Hi, my name is ${name} and I'm ${age}. I was born in ${yob}.`;
 }
 
 try {
-  const greeting1 = createGreeting('Nick', -2);
+  const greeting1 = createGreeting('Life', 42);
   console.log(greeting1);
 }
 catch (error) {
-  console.log(error);
+  console.log(error.message);
 }
