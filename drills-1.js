c79f1cceb309d53a2324864981b09de1675de465
DanielJSottile <danielsottile@gmail.com>
2020-03-16 15:15:31 -0400

    6

--- drills-1.js
+++ drills-1.js
@@ -1,8 +1,12 @@
 'use strict';
 
+function getYearOfBirth(age) {
+  return 2020 - age;
+}
+
 function createGreeting(name, age) {
-  const yearOfBirth = 2020 - age;
-  return `Hi, my name is ${name} and I'm ${age}. I was born in ${yearOfBirth}.`;
+  const yob = getYearOfBirth;
+  return `Hi, my name is ${name} and I'm ${age}. I was born in ${yob(age)}.`;
 }
 
 const greeting1 = createGreeting();
