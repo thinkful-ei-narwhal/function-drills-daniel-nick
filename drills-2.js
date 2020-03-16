18feff229a40333eed5eb8c049ee340c915b68cb
nevelson <nevelson92@gmail.com>
2020-03-16 17:06:38 -0400

    Rock paper scissors

--- drills-2.js
+++ drills-2.js
@@ -52,7 +52,7 @@ function decode(codedSentence) {
 
 // days in a month
 
-function daysInMonth(month,leapYear=false) {
+function daysInMonth(month, leapYear = false) {
   let thirtyOne = ['January', 'March', 'May', 'July', 'August', 'October', 'December'];
   let thirty = ['April', 'June', 'September', 'November'];
 
@@ -60,20 +60,62 @@ function daysInMonth(month,leapYear=false) {
     throw 'Must provide a valid month';
   }
 
-  switch(true) {
-  case thirtyOne.includes(month):
-    return `${month} has 31 days`;
-    
-  case thirty.includes(month):
-    return `${month} has 30 days`;
-    
-  case month === 'February':
-    if (leapYear === true) {
-      return 'February has 29 days';
-    } else {
-      return 'February has 28 days';
-    }
+  switch (true) {
+    case thirtyOne.includes(month):
+      return `${month} has 31 days`;
+
+    case thirty.includes(month):
+      return `${month} has 30 days`;
+
+    case month === 'February':
+      if (leapYear === true) {
+        return 'February has 29 days';
+      } else {
+        return 'February has 28 days';
+      }
   }
 }
 
-//
+//Rock paper scissors
+function rockPaperScissor(playerNum) {
+  //1 rock
+  //2 paper
+  //3 scissors
+  if (playerNum < 1 || playerNum > 3) {
+    throw 'Need a number between one and three.';
+  }
+
+  const randomNo = Math.floor(Math.random() * 3) + 1;
+  console.log(randomNo);
+
+  const playerWinner = 'player wins';
+  const compWinner = 'computer wins';
+
+  if (playerNum === randomNo) {
+    return 'tie';
+  }
+  else if (playerNum === 1) {
+    if (randomNo === 3) {
+      return playerWinner;
+    }
+    else {
+      return compWinner;
+    }
+  }
+  else if (playerNum === 2) {
+    if (randomNo === 1) {
+      return playerWinner;
+    }
+    else {
+      return compWinner;
+    }
+  }
+  else if (playerNum === 3) {
+    if (randomNo === 2) {
+      return playerWinner;
+    }
+    else {
+      return compWinner;
+    }
+  }
+}
